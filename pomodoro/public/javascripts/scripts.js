794113fcdcf7515c20e7a86905c7bc8ff28bc4f3
KevinLin1120 <kevinlin1120a@gmail.com>
2022-12-30 11:14:41 +0800

    Add pre/next song btn and func

--- pomodoro/public/javascripts/scripts.js
+++ pomodoro/public/javascripts/scripts.js
@@ -1,5 +1,50 @@
+// Get playing status
+function getCurrentState(player, which){
+    player.getCurrentState().then(state => {
+        if (!state) {
+          console.error('User is not playing music through the Web Playback SDK');
+          return;
+        }
+    
+        var current_track = state.track_window.current_track;
+        var next_track = state.track_window.next_tracks[0];
+        var previous_track = state.track_window.previous_tracks[1];
+        
+        // Update playing song name
+        document.getElementById('playing-name').innerText = current_track.name;
+        // Update playing album img
+        switch(which){
+            // Previous
+            case 0:
+                document.getElementById('playing-name').innerText
+                 = previous_track.name;
+                document.getElementById('album').src
+                 = previous_track.album.images[1].url;
+                break;
+            // Current
+            case 1:
+                document.getElementById('playing-name').innerText
+                 = current_track.name;
+                document.getElementById('album').src
+                 = current_track.album.images[1].url;
+                break;
+            // Next
+            case 2:
+                document.getElementById('playing-name').innerText
+                 = next_track.name;
+                document.getElementById('album').src
+                 = next_track.album.images[1].url;
+                break;
+        }
+        
+        console.log(state.track_window);
+        console.log('Currently Playing', current_track);
+        console.log('Playing Next', next_track);
+    });
+}
+
 window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = 'BQCAk27lBd64HVIdUpsKXrQ12bf2-g_6bsQlQxSFtFGLk09Dm-4f19ME9U9KOwPqig6gsN_jFlXtKYjf2tM-tLauNEG0eUvQezyBBkzakC5N2rJllOn4vRSwlaIX_WKl0-zPkG_4ihuUg9WujQY9-5DaMYSM2u5mAkYg8593w0fMnNxqOyu9M2rom6Q5W0eTZ6yqJ0F8UsBKIRJ3qPoDpKCIWaQ';
+    const token = 'BQAmh8zg5PC_Se35lLCy0o57yFN7DgJ3PmGTaFZ9L2ltfGHeu_L8a1ZydQhlq579zF5WhUaS6pbpzR8kbWI1_fKP_7j26it9BiFUraD8Hl5NHbj05SPIZnLN5WVdWeQtQyJKNIW0Nt4CDGCsxOwok7M_ViGnkFWutfF3owHmVFU4711XpAn8Zx-qbnoQTGfiP52D3SOYNoG1p-oSL0fHmkoomJ8';
     const player = new Spotify.Player({
       name: 'Web Playback SDK Quick Start Player',
       getOAuthToken: cb => { cb(token); },
@@ -32,53 +77,45 @@ window.onSpotifyWebPlaybackSDKReady = () => {
         console.log('Autoplay is not allowed by the browser autoplay rules');
     });
 
-    // Get playing status
-    // player.getCurrentState().then(state => {
-    //     if (!state) {
-    //       console.error('User is not playing music through the Web Playback SDK');
-    //       return;
-    //     }
-      
-    //     var current_track = state.track_window.current_track;
-    //     var next_track = state.track_window.next_tracks[0];
-      
-    //     console.log('Currently Playing', current_track);
-    //     console.log('Playing Next', next_track);
-    //   });
-
     // Play toggle
     document.getElementById('togglePlay').onclick = function() {
         player.togglePlay().then(() => {
             console.log('Toggled playback!');
-            document.getElementById('play').src = document.getElementById('play').src == 'http://127.0.0.1:3000/images/icons/icons8-play-button-circled.svg' ?
+            document.getElementById('play').src = document.getElementById('play').src ==
+             'http://127.0.0.1:3000/images/icons/icons8-play-button-circled.svg' ?
             '/images/icons/pause.svg' : '/images/icons/icons8-play-button-circled.svg';
+        }).then(() => {
+             // Update playing content info
+            getCurrentState(player, 1);
         });
+        // Update playing content info
+        getCurrentState(player, 1);
+       
     };
     
     // Play previous song
     document.getElementById('previous').onclick = function(){
         player.previousTrack().then(() => {
             console.log('Set to previous track!');
-          });
+        }).then(() => {
+            // Update playing content info
+            getCurrentState(player, 0);
+        });
+        // Update playing content info
+        getCurrentState(player, 0);
     }
 
     // Play next song
     document.getElementById('next').onclick = function(){
         player.nextTrack().then(() => {
             console.log('Skipped to next track!');
+        }).then(() => {
+            // Update playing content info
+            getCurrentState(player, 2);
         });
+        // Update playing content info
+        getCurrentState(player, 2);
     }
-
-    
-
-    // document.getElementById('auth').onclick = function() {
-    //     player.activateElement();
-    //     console.log("auth");
-    // };
-    // myActivateElementButton.addEventListener('click', () => {
-    //     // The player is activated. The player will keep the playing state once the state is transferred from other applications.
-    //     player.activateElement();
-    //   });
     player.connect();
 }
 
